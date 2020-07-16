524eb1cb085ff531d24b57ee3d11246394a80921
lieeber <lieeber1@gmail.com>
2020-07-17 00:52:58 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//app.js
+App({
+  serverUrl: "http://127.0.0.1:8080",
+  userInfo: null,
+  setGlobalUserInfo: function(user) {
+    wx.setStorageSync("userInfo", user);
+  },
+
+  getGlobalUserInfo: function () {
+    return wx.getStorageSync("userInfo");
+  },
+  onLaunch: function () {
+  
+  },
+})
