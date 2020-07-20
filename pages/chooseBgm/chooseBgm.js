cfcb53ee4d24c0d82a81397d1c4986f74bbc4e53
lieeber <lieeber@foxmail.com>
2020-07-20 21:54:59 +0800

    Add:背景音乐列表界面。。

--- pages/chooseBgm/chooseBgm.js
+++ pages/chooseBgm/chooseBgm.js
@@ -2,13 +2,45 @@ const app = getApp()
 
 Page({
     data: {
+      audioItem:[],
       bgmList: [],
-      serverUrl: "",
+      serverUrl: app.serverUrl,
       videoParams: {}
     },
 
     onLoad: function (params) {
-      
+      const me = this;
+      wx.showLoading({
+        title: '正在加载背景音乐列表'
+      })
+      wx.request({
+        url: app.serverUrl + "/bgm/list",
+        method:"GET",
+        success(res){
+          // console.log("bgm====="+JSON.stringify(res.data))
+          wx.hideLoading();
+          if(res.data.status === 200){
+            const bgmList = res.data.data;
+            me.setData({
+              bgmList:bgmList,
+            })
+          }
+        }
+      })
+    },
+    tapDisplay: function(e){
+        var currentIAudio = e.target
+        for (let i = 0; i <this.data.audioItem.size; i++) {
+            let newVar = this.data.audioItem.get(i);
+            if (currentIAudio !== newVar) {
+                newVar.myMp3.pause()
+            }
+        }
+    },
+    IAudioReady: function(e){
+       var iaudio = e.target
+        this.data.audioItem.put(iaudio)
+        console.log("获取到了这个iaudio==============:"+JSON.stringify(iaudio))
     },
 
     upload: function(e) {
--- pages/component/iaudio/iaudio.js
+++ pages/component/iaudio/iaudio.js
@@ -22,6 +22,9 @@ Component({
         var myMp3 = wx.createInnerAudioContext();
         myMp3.autoplay = false;
         myMp3.src = this.data.src;
+        this.triggerEvent("onIAudioReady", {
+            myMp3: this.data.myMp3
+        })
         this.setData({
             myMp3: myMp3
         })
@@ -32,15 +35,25 @@ Component({
      */
     methods: {
         display() {
+            const me = this;
             const isPlaying = this.data.isPlaying;
+            var myEventOption = {} // 触发事件的选项
             if (!isPlaying) {
                 this.data.myMp3.play();
+                this.triggerEvent("tapDisplay", {
+                    isPlaying: true,
+                    myMp3: me.data.myMp3
+                }, myEventOption)
                 this.setData({
                     isPlaying: true,
                     ico: "pause"
                 })
             } else {
                 this.data.myMp3.pause();
+                this.triggerEvent("tapDisplay", {
+                    isPlaying: false,
+                    myMp3: me.data.myMp3
+                }, myEventOption)
                 this.setData({
                     isPlaying: false,
                     ico: "play"
