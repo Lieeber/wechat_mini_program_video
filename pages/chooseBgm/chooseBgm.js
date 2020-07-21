f5288b4f1aefd880a060d4bac6f2a9947c9c79e5
lieeber <lieeber@foxmail.com>
2020-07-21 15:26:04 +0800

    Add:添加了一个功能，在点击播放某个音乐的时候，能够暂停播放正在播放中的其他音乐。

--- pages/chooseBgm/chooseBgm.js
+++ pages/chooseBgm/chooseBgm.js
@@ -2,48 +2,43 @@ const app = getApp()
 
 Page({
     data: {
-      audioItem:[],
-      bgmList: [],
-      serverUrl: app.serverUrl,
-      videoParams: {}
+        audioItem: [],
+        bgmList: [],
+        serverUrl: app.serverUrl,
+        videoParams: {}
     },
 
     onLoad: function (params) {
-      const me = this;
-      wx.showLoading({
-        title: '正在加载背景音乐列表'
-      })
-      wx.request({
-        url: app.serverUrl + "/bgm/list",
-        method:"GET",
-        success(res){
-          // console.log("bgm====="+JSON.stringify(res.data))
-          wx.hideLoading();
-          if(res.data.status === 200){
-            const bgmList = res.data.data;
-            me.setData({
-              bgmList:bgmList,
-            })
-          }
-        }
-      })
+        const me = this;
+        wx.showLoading({
+            title: '正在加载背景音乐列表'
+        })
+        wx.request({
+            url: app.serverUrl + "/bgm/list",
+            method: "GET",
+            success(res) {
+                wx.hideLoading();
+                if (res.data.status === 200) {
+                    const bgmList = res.data.data;
+                    me.setData({
+                        bgmList: bgmList,
+                    })
+                }
+            }
+        })
     },
-    tapDisplay: function(e){
-        var currentIAudio = e.target
-        for (let i = 0; i <this.data.audioItem.size; i++) {
-            let newVar = this.data.audioItem.get(i);
-            if (currentIAudio !== newVar) {
-                newVar.myMp3.pause()
+    tapDisplay: function (e) {
+        for (let i = 0; i < this.data.audioItem.length; i++) {
+            console.log(this.data.audioItem[i].id +"====="+e.target.id)
+            if (this.data.audioItem[i].id !== e.target.id) {
+                this.data.audioItem[i].pauseAudio(false)
             }
         }
     },
-    IAudioReady: function(e){
-       var iaudio = e.target
-        this.data.audioItem.put(iaudio)
-        console.log("获取到了这个iaudio==============:"+JSON.stringify(iaudio))
-    },
-
-    upload: function(e) {
+    IAudioReady: function (e) {
+        let id = e.target.id;
+       let myaudio  = this.selectComponent("#"+id)
+        this.data.audioItem.push(myaudio);
     }
 })
 
--- pages/component/iaudio/iaudio.js
+++ pages/component/iaudio/iaudio.js
@@ -1,32 +1,25 @@
 // pages/component/iaudio.js
 Component({
-    /**
-     * 组件的属性列表
-     */
     properties: {
         src: String,
         songName: String,
         singer: String,
         duration: String
     },
-
-    /**
-     * 组件的初始数据
-     */
     data: {
         myMp3: {},
         isPlaying: false,
         ico: "play"
     },
     ready: function () {
-        var myMp3 = wx.createInnerAudioContext();
+        let myMp3 = wx.createInnerAudioContext();
         myMp3.autoplay = false;
         myMp3.src = this.data.src;
-        this.triggerEvent("onIAudioReady", {
-            myMp3: this.data.myMp3
-        })
         this.setData({
             myMp3: myMp3
+        })
+        this.triggerEvent("onIAudioReady", {
+
         })
     },
 
@@ -37,27 +30,30 @@ Component({
         display() {
             const me = this;
             const isPlaying = this.data.isPlaying;
-            var myEventOption = {} // 触发事件的选项
             if (!isPlaying) {
-                this.data.myMp3.play();
-                this.triggerEvent("tapDisplay", {
-                    isPlaying: true,
-                    myMp3: me.data.myMp3
-                }, myEventOption)
-                this.setData({
-                    isPlaying: true,
-                    ico: "pause"
-                })
+                me.playAudio(true);
             } else {
-                this.data.myMp3.pause();
+                me.pauseAudio(true);
+            }
+        }, playAudio(trigger) {
+            this.data.myMp3.play();
+            this.setData({
+                isPlaying: true,
+                ico: "pause"
+            })
+            if (trigger) {
                 this.triggerEvent("tapDisplay", {
-                    isPlaying: false,
-                    myMp3: me.data.myMp3
-                }, myEventOption)
-                this.setData({
-                    isPlaying: false,
-                    ico: "play"
-                })
+                });
+            }
+        }, pauseAudio(trigger) {
+            this.data.myMp3.pause();
+            this.setData({
+                isPlaying: false,
+                ico: "play"
+            })
+            if (trigger) {
+                this.triggerEvent("tapDisplay", {
+                });
             }
         }
     }
