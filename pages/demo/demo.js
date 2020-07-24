260ee409be70e2109af8583a7c329ca3b349755f
lieeber <lieeber@foxmail.com>
2020-07-24 21:32:36 +0800

    Update:some demo

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -1,21 +1,23 @@
 Page({
     data: {
-        todolist:[false,false,true,true],
+        todolist: [false, false, true, true],
         show: false,
-        list:["fdfd",233,true,{bb:"我是对象"}],
-        iValue:"默認數據"
+        list: ["fdfd", 233, true, {bb: "我是对象"}],
+        iValue: "默認數據"
     },
     onLoad: function (options) {
 
     },
     clickView2: function (e) {
-        this.data.show = !this.data.show;
-        console.log(this.data.show)
-        this.setData({
-            show: this.data.show
-        })
+        // this.data.show = !this.data.show;
+        // console.log(this.data.show)
+        // this.setData({
+        //     show: this.data.show
+        // })
+        let todo1 = this.selectComponent("#todo1");
+        console.log(todo1.data.iText)
     },
-    clickItem(event){
+    clickItem(event) {
         for (let i = 0; i < this.data.todolist.length; i++) {
             this.data.todolist[i] = true
         }
@@ -23,7 +25,7 @@ Page({
         this.setData({
             todolist: this.data.todolist
         })
-        console.log("我是通過組件觸發的"+event.detail.index)
+        console.log("我是通過組件觸發的" + event.detail.index)
 
     }
 });
